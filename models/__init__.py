d68aca3a69e7e1de6ac0985dcbda3ce876b22ee4
pida0 <shuying97@163.com>
2022-07-12 23:25:56 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,4 @@
-'''
-Author: sy
-Date: 2021-03-31 15:12:55
-LastEditors: pida0
-LastEditTime: 2022-07-12 16:46:30
-Description: file content
-'''
+
 
 from .s3net_5branches import get_model_5b, ClsNet, Discriminators
 from .trans_s3net_5b import get_down_model_5b
