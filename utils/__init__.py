114d98b7af2cd8888a1b15f585525a833b325ed6
pida0 <shuying97@163.com>
2022-07-12 23:25:31 +0800

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,4 @@
-'''
-Author: sy
-Date: 2021-03-31 16:04:59
-LastEditors: your name
-LastEditTime: 2021-04-29 15:46:27
-Description: file content
-'''
+
 
 from .logger import record, show_eval_result
 from .lr import lambda_warm_up, lambda_warm_up_2
